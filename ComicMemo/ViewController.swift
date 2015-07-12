60749c44e39cdc69a0d4fd1d67210c164a6bfe0e
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2015-07-13 00:17:20 +0900

    マンガメモ画面レイアウト作成

--- ComicMemo/ViewController.swift
+++ ComicMemo/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // 行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // セルの設定
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("memoCell") as! UITableViewCell
+        
+        // tag1を取得
+        var titleText = cell.viewWithTag(1) as! UITextField
+        titleText.text = "ワンピース"
+        
+        // tag2を取得
+        var num = cell.viewWithTag(2) as! UILabel
+        num.text = "\(indexPath.row)"
+        
+        var addStepper = cell.viewWithTag(4) as! UIStepper
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
