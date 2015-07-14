90c15d8b941c4634f78525016d4e3a6392088cb5
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2015-07-14 23:58:09 +0900

    テーブルを追加、削除する機能を追加

--- ComicMemo/ViewController.swift
+++ ComicMemo/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    let myItems: NSMutableArray = ["ワンピース", "BREACH", "鋼の錬金術師"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func tapAdd(sender: AnyObject) {
+        // myItemsに追加.
+        myItems.addObject("タイトルを入力")
+        
+        // TableViewを再読み込み.
+        tableView.reloadData()
+    }
+    
+    @IBAction func tapEdit(sender: AnyObject) {
+        if editing {
+            super.setEditing(false, animated: true)
+            tableView.setEditing(false, animated: true)
+        } else {
+            super.setEditing(true, animated: true)
+            tableView.setEditing(true, animated: true)
+        }
+    }
+    
     // 行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return myItems.count
     }
     
     // セルの設定
@@ -26,7 +47,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         // tag1を取得
         var titleText = cell.viewWithTag(1) as! UITextField
-        titleText.text = "ワンピース"
+        titleText.text = "\(myItems[indexPath.row])"
         
         // tag2を取得
         var num = cell.viewWithTag(2) as! UILabel
@@ -34,6 +55,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         var addStepper = cell.viewWithTag(4) as! UIStepper
         return cell
+    }
+
+    /*
+    Cellを挿入または削除しようとした際に呼び出される
+    */
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // 削除のとき.
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            // 指定されたセルのオブジェクトをmyItemsから削除する.
+            myItems.removeObjectAtIndex(indexPath.row)
+            
+            // TableViewを再読み込み.
+            tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
